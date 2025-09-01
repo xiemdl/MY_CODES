cd24a3d400eb281a639a77db6e7f8d7291b6f0a4
xiemdl <shielamadelanonuevodomingo@1804gmail.com>
2025-09-01 11:47:58 +0800

    main

--- PYTHON/calculator.py
+++ PYTHON/calculator.py
@@ -5,7 +5,7 @@ num1 = input("| Enter 1st number: ")
 print("-------------------------------------------------")
 num2 = input("| Enter 2nd number: ")
 print("-------------------------------------------------")
-operation = input("| Enter an operation (+, -, *, /): ")
+operation = input("| Enter an operation (+, -, *, /,//, %, **): ")
 
 if operation == "+":
     result = float(num1) + float(num2)
@@ -15,11 +15,15 @@ elif operation == "*":
     result = float(num1) * float(num2)
 elif operation == "/":
     result = float(num1) / float(num2)
+elif operation == "//":
+    result = float(num1) // float(num2)
+elif operation == "%":
+    result = float(num1) % float(num2)
+elif operation == "**":
+    result = float(num1) ** float(num2)
 else:
     result = "Operator is not recognized"
 print("-------------------------------------------------")
 print("|RESULT:", num1 , operation, num2, "=" , result,"                        |")
 print("-------------------------------------------------")
 
-for c in range(5):
-    print("|", c, "|")
