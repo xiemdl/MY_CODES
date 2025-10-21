24fd3951e3316bffe4b201a9cd07b670230ee1cd
xiemdl <shielamadelanonuevodomingo@1804gmail.com>
2025-10-21 09:12:27 +0800

    main

--- JAVASCRIPT/DOM_mnpltn.js
+++ JAVASCRIPT/DOM_mnpltn.js
@@ -1,9 +1,11 @@
+// find the element in the html using id
 let name = document.getElementById("name");
 let hobby = document.getElementById("hobby");
 let color = document.getElementById("color");
 let button = document.getElementById("Update");
 let button1 = document.getElementById("Reset");
 
+//add event listerner for action (sample: button click)
 button.addEventListener("click", () => {
     name.textContent = "Name: " + prompt("Enter your name:");
     hobby.textContent = "Hobby: " + prompt("Enter your hobby:");
@@ -14,6 +16,7 @@ button.addEventListener("click", () => {
 
 });
 
+// this add event listerner for reset button
 button1.addEventListener("click",() =>{
     name.textContent ="Name:";
     hobby.textContent ="Hobby:";        
@@ -23,10 +26,12 @@ button1.addEventListener("click",() =>{
 
 });
 
+// it loads the stored data from local storage when the page is loaded
 const storedName = localStorage.getItem("name");
 const storedHobby = localStorage.getItem("hobby");
 const storedColor = localStorage.getItem("color");
 
+// set the text content of the elements if data exists
 if (storedName) name.textContent = storedName;
 if (storedHobby) hobby.textContent = storedHobby;
 if (storedColor) color.textContent = storedColor;
